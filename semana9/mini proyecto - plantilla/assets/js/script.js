6c21ed4f8a850089e46cb582b5c40fe11012368b
Brizett <florenziatrujillo@gmail.com>
2024-08-29 20:50:16 -0500

    avance

--- semana9/mini proyecto - plantilla/assets/js/script.js
+++ semana9/mini proyecto - plantilla/assets/js/script.js
@@ -1,8 +1,9 @@
 // Mini Proyecto 
 // Inicializamos nuestro arreglo 
-const cars = ["bmw", "volvo", "saab", "ford", "fiat", "audi"];
+let cars = ["bmw", "volvo", "saab", "ford", "fiat", "audi"];
 
 // Metodos de array
+// Push
 let btnPush = document.getElementById("btn-push");
 //Asignamos la función al botón
 btnPush.onclick = funcionPush;
@@ -12,6 +13,67 @@ function funcionPush() {
     cars.push("lexus");
     // Cargamos las imagenes nuevamente
     mostrarImagenes();
+}
+
+// Pop
+let btnPop = document.getElementById("btn-pop");
+//Asignamos la función al botón
+btnPop.onclick = funcionPop;
+
+function funcionPop() {
+    if (cars.length > 0) {
+    // Actualizamos el array
+    cars.pop();
+    // Cargamos las imagenes nuevamente
+    mostrarImagenes();
+    } else {
+        alert ("No se puede eliminar más elementos ya que el arreglo está vacío")
+    }
+}
+
+// Slice
+let btnSlice = document.getElementById("btn-slice");
+btnSlice.onclick = funcionSlice;
+
+function funcionSlice () {
+    let begin = document.getElementById("begin").value;
+    let end = document.getElementById("end").value;
+
+    // Nos preguntamos si no son strings vacías
+    if (begin != "" && end != "") {
+        // Actualizamos el array
+        cars = cars.slice(begin, end);
+        // Cargamos las imágenes nuevamente
+        mostrarImagenes();
+    } else {
+        alert ("Los campos no pueden estar vacíos");
+    }
+}
+
+// Shift
+let btnShift = document.getElementById("btn-shift");
+btnShift.onclick = funcionShift;
+
+function funcionShift() {
+    if (cars.length > 0) {
+    // Actualizamos el array
+    cars.shift();
+    // Cargamos las imagenes nuevamente
+    mostrarImagenes();
+    } else {
+        alert ("No se puede eliminar más elementos ya que el arreglo está vacío")
+    }
+}
+    
+// Unshift
+let btnUnshift = document.getElementById("btn-unshift");
+btnUnshift.onclick = funcionUnshift;
+
+function funcionUnshift() {
+     // Actualizamos el array
+     cars.unshift("honda");
+     // Cargamos las imagenes nuevamente
+     mostrarImagenes();
 }
 
 // Esta función carga las imagenes en el elemento demo
---
+++ semana9/s42/assets/js/script.js
@@ -0,0 +1,18 @@
+let letras = ["a", "b", "c", "a", "d", "e", "f", "a", "g", "h"];
+console.log(letras);
+
+console.log("indexOf");
+
+let indice = letras.indexOf("a");
+console.log(indice);
+console.log(letras.indexOf("d"));
+
+console.log(letras.indexOf("a",4));
+
+console.log(letras.indexOf("x"));
+
+console.log("lastIndexOf");
+
+console.log(letras.lastIndexOf("a"));
+
+console.log(letras.lastIndexOf("a",1));
